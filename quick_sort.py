478388341709189d477a478c93adfc96167a458c
vasanth <59464869+Git-Vasanth@users.noreply.github.com>
2025-08-27 12:48:25 -0500

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+un_orderlist = [3,2,0,5,1,8,7,6,9,4]
+
+
+last_value = un_orderlist[-1]
+
+for i in range(0 , len(un_orderlist)):
+
+    for j in range(0 , len(un_orderlist) - 1):
+
+        if un_orderlist[j] <= last_value :
+
+            
